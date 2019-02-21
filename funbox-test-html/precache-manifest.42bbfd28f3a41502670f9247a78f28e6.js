5a08a1ac2e6eed796e5d245fec8aaec7639d804d
LexRiver <lexriver@gmail.com>
2019-02-21 12:38:28 +0300

    funbox-test-html

---
+++ funbox-test-html/precache-manifest.42bbfd28f3a41502670f9247a78f28e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "acfeac286d2174aa71ffdb73112732e1",
+    "url": "/funbox-test-html/static/media/pattern.acfeac28.jpg"
+  },
+  {
+    "revision": "25f881abfb28e2b971a213d8b2b10b7d",
+    "url": "/funbox-test-html/static/media/cat-small.25f881ab.png"
+  },
+  {
+    "revision": "d9d2ef1a92f5192b3a379dde277c4868",
+    "url": "/funbox-test-html/static/media/Exo2.0-Thin.d9d2ef1a.eot"
+  },
+  {
+    "revision": "3dc0b34835673a2b0fa861b371fc41cb",
+    "url": "/funbox-test-html/static/media/Exo2.0-Thin.3dc0b348.woff"
+  },
+  {
+    "revision": "1e7bac796c0514c0b87f50b237cccfaf",
+    "url": "/funbox-test-html/static/media/Exo2.0-Thin.1e7bac79.ttf"
+  },
+  {
+    "revision": "edca5c6819c81b452a05",
+    "url": "/funbox-test-html/static/js/runtime~main.edca5c68.js"
+  },
+  {
+    "revision": "f108c0ce5f8cc6daecf9",
+    "url": "/funbox-test-html/static/js/main.f108c0ce.chunk.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/funbox-test-html/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "f108c0ce5f8cc6daecf9",
+    "url": "/funbox-test-html/static/css/main.451c4832.chunk.css"
+  },
+  {
+    "revision": "cfd8ec3eb0fbd8764a52ec7901ca2041",
+    "url": "/funbox-test-html/index.html"
+  }
+];
